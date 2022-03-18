188ec0e1fe4d2dd4c084b7559c26a40f92b2d987
ashrafulrahmansakil <sakil55183@gmail.com>
2022-03-18 12:01:29 +0600

    project-int

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// const humbarger = document.querySelector("#btn");
+// const header__Navbar= document.querySelector("#nav__item");
+// humbarger.addEventListener("click",()=>{
+//     humbarger.classList.toggle("active");
+//     header__Navbar.classList.toggle("active");
+// })
+
+// document.querySelectorAll(".nav__link"),forEach(n => n.addEventListener("click",()=>{
+//     humbarger.classList.remove("active");
+//     header__Navbar.classList.remove("active");
+// }))
