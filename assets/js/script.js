c398a85d263743b6951c550d19e82b1b98dbdc8e
ashrafulrahmansakil <sakil55183@gmail.com>
2023-02-23 13:00:07 +0600

    project-int

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,25 @@
-// const humbarger = document.querySelector("#btn");
-// const header__Navbar= document.querySelector("#nav__item");
-// humbarger.addEventListener("click",()=>{
-//     humbarger.classList.toggle("active");
-//     header__Navbar.classList.toggle("active");
-// })
+/*
+ * hambarger open close
+ */
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector("ul");
+const links = document.querySelectorAll("li");
 
-// document.querySelectorAll(".nav__link"),forEach(n => n.addEventListener("click",()=>{
-//     humbarger.classList.remove("active");
-//     header__Navbar.classList.remove("active");
-// }))
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+  hamburger.classList.toggle("open");
+});
+
+/*
+ * hamburger sidebar
+ */
+
+// toggle
+const sidebar = document.querySelector(".navbar__menu");
+
+hamburger.addEventListener("click", function () {
+    sidebar.classList.toggle("sidebar-hide")
+})
